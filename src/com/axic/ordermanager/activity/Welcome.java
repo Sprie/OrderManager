b972d7feb7dd1e4dfbd21a8d0312f7f886a88720
Sprie <zhouaxic@gmail.com>
2016-03-06 17:48:36 +0800

    version 1 0.0.05 在启动页面增加了版本号的显示

--- src/com/axic/ordermanager/activity/Welcome.java
+++ src/com/axic/ordermanager/activity/Welcome.java
@@ -3,16 +3,21 @@ package com.axic.ordermanager.activity;
 import com.axic.ordermanager.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Welcome extends Activity {
 
+	TextView vs;
 	private ImageView welcomeImg = null;
 	
 	@Override
@@ -20,10 +25,13 @@ public class Welcome extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.welcomeimg);
+		//启动页面显示版本名
+		vs = (TextView) findViewById(R.id.version);
+		vs.setText("version " + getVersion());
 		
 		welcomeImg = (ImageView) findViewById(R.id.welcome_img);
 		AlphaAnimation anima = new AlphaAnimation(0.3f, 1.0f);
-		anima.setDuration(2500);
+		anima.setDuration(3000);
 		welcomeImg.startAnimation(anima);
 		anima.setAnimationListener(new AnimationListener() {
 			
@@ -31,6 +39,8 @@ public class Welcome extends Activity {
 			public void onAnimationStart(Animation animation) {
 				// TODO Auto-generated method stub
 				welcomeImg.setBackgroundResource(R.drawable.welcome);
+				
+				
 			}
 			
 			@Override
@@ -48,5 +58,16 @@ public class Welcome extends Activity {
 			}
 		});
 		
+	}
+	//获取版本号
+	public String getVersion() {
+		try {
+			PackageInfo pi = 
+					this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+			return pi.versionName;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+			return this.getString(R.string.version_unknown);
+		}
 	}
 }
